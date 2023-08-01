e93ed3de92cca2c62b6bd89e4b97ad0425575240
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2023-08-01 12:00:33 +0530

    style: static header

---
+++ app/index.js
@@ -0,0 +1,40 @@
+import { StyleSheet, Text, View } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+
+export default function Page() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View>
+          <Text style={styles.title}>My model S</Text>
+          <Text style={styles.subtitle}>Parked</Text>
+        </View>
+        <FontAwesome name="user-circle" size={30} color="gray" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+    padding: 24,
+    backgroundColor: "#161818",
+  },
+  header: {
+    marginTop: 50,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#eee",
+    marginBottom: 8,
+  },
+  subtitle: {
+    fontWeight: 500,
+    color: "gray",
+  },
+});
