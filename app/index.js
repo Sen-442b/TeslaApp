e0b9003ce3eea2a1fe2ebd3bfd2b7ed18578dde6
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2023-08-01 13:23:24 +0530

    style: controls UI added

--- app/index.js
+++ app/index.js
@@ -1,5 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
-import { FontAwesome } from "@expo/vector-icons";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+import {
+  FontAwesome,
+  Entypo,
+  MaterialCommunityIcons,
+  FontAwesome5,
+  Ionicons,
+  MaterialIcons,
+} from "@expo/vector-icons";
+import car from "../assets/images/car.png";
 
 export default function Page() {
   return (
@@ -10,6 +19,21 @@ export default function Page() {
           <Text style={styles.subtitle}>Parked</Text>
         </View>
         <FontAwesome name="user-circle" size={30} color="gray" />
+      </View>
+      <Image source={car} style={styles.image} resizeMode="contain" />
+      <View style={styles.controls}>
+        <Entypo name="lock" size={26} color="gray" />
+        <MaterialCommunityIcons name="fan" size={26} color="gray" />
+        <FontAwesome5 name="bolt" size={26} color="gray" />
+        <Ionicons name="car-sport-sharp" size={26} color="gray" />
+      </View>
+      <View style={styles.optionRow}>
+        <View style={styles.optionRowChild}>
+          <MaterialCommunityIcons name="car" size={26} color="gray" />
+
+          <Text style={styles.optionText}>Controls</Text>
+        </View>
+        <MaterialIcons name="keyboard-arrow-right" size={26} color="gray" />
       </View>
     </View>
   );
@@ -26,6 +50,7 @@ const styles = StyleSheet.create({
     marginTop: 50,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
   },
   title: {
     fontSize: 24,
@@ -36,5 +61,26 @@ const styles = StyleSheet.create({
   subtitle: {
     fontWeight: 500,
     color: "gray",
+  },
+  image: { width: "100%", height: 300 },
+  controls: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  optionRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  optionRowChild: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  optionText: {
+    color: "#eee",
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 10,
   },
 });
